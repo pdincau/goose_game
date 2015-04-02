32c4e5a37099f3a8d5b951e213fcd5f458fe3aef
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-04-02 17:14:33 +0200

    Creating player_created event in player.erl

--- src/player.erl
+++ src/player.erl
@@ -1,27 +1,43 @@
 -module(player).
--export([start/1, init/1]).
+-export([start/1, init/0]).
 
--record(state, {name}).
+-record(state, {name, events}).
+-record(player_created, {name, date_created}).
 
 start(Name) ->
-    Pid = spawn(?MODULE, init, [Name]),
+    Pid = spawn(?MODULE, init, []),
     Pid ! {attempt_command, {create, Name}}.
 
-init(Name) ->
-    loop(#state{name=Name}).
+init() ->
+    loop(#state{events=[]}).
 
 loop(State) ->
     receive
         {attempt_command, Command} ->
-            attempt_command(Command),
-            loop(State);
+            NewState = attempt_command(Command, State),
+            loop(NewState);
         Msg ->
             Name = State#state.name,
+            Events = State#state.events,
             io:format("Player ~p received message ~p~n", [Name, Msg]),
+            io:format("Player ~p has events ~p~n", [Name, Events]),
             loop(State)
     end.
 
-attempt_command({create, Name}) ->
-    gproc:reg({n, l, {?MODULE, Name}}),
-    gproc:await({n,l, {?MODULE, Name}}).
+attempt_command({create, Name}, State) ->
+    Event = #player_created{name=Name, date_created=erlang:localtime()},
+    apply_new_event(Event, State).
 
+apply_new_event(Event, State) ->
+    NewState = apply_event(Event, State),
+    NewEvents = [Event | NewState#state.events],
+    NewState#state{events=NewEvents}.
+
+apply_event(#player_created{name=Name, date_created=_DateCreated}, State) ->
+    case gproc:where({n,l, {player, Name}}) of
+        undefined ->
+            gproc:reg({n, l, {player, Name}}),
+            gproc:await({n,l, {player, Name}});
+        _ -> ok
+    end,
+    State#state{name=Name}.
