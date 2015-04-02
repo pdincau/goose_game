b65d2985d775ca21c6aa0969001e2bb7656b8bef
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-04-02 14:48:01 +0200

    Excracting command to attemp_command/1

--- src/player.erl
+++ src/player.erl
@@ -3,16 +3,19 @@
 
 start(Name) ->
     Pid = spawn(?MODULE, loop, [Name]),
-    Pid ! {create, Name}.
+    Pid ! {attempt_command, {create, Name}}.
 
 loop(Name) ->
     receive
-        {create, Name} ->
-            gproc:reg({n, l, {?MODULE, Name}}),
-            gproc:await({n,l, {?MODULE, Name}}),
+        {attempt_command, Command} ->
+            attempt_command(Command),
             loop(Name);
         Msg ->
             io:format("Player ~p received message ~p~n", [Name, Msg]),
             loop(Name)
     end.
+
+attempt_command({create, Name}) ->
+    gproc:reg({n, l, {?MODULE, Name}}),
+    gproc:await({n,l, {?MODULE, Name}}).
 
