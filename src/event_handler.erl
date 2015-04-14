364dd3a91aa3d04ff33cbbd918a27f6679f63bc7
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-04-14 15:56:12 +0200

    Adding simple handling of player_created event

--- src/event_handler.erl
+++ src/event_handler.erl
@@ -7,12 +7,17 @@
          handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {}).
+-record(player_created, {name, date_created}).
 
 subscribe() ->
     event_bus:add_handler(?MODULE, []).
 
 init([]) ->
     {ok, #state{}}.
+
+handle_event(#player_created{name=Name, date_created=_}, State) ->
+    io:format("Giocatore ~p si unisce alla partita.~n", [Name]),
+    {ok, State};
 
 handle_event(Event , State) ->
     io:format("Received unknown event: ~p~n", [Event]),
